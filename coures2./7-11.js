ce029d5e345aa6636041898e2a23a6a1d10a6fd0
LightningDash6 <daniel@behuman.org>
2017-05-29 14:52:25 +0100

    Update 7-11.js

--- coures2./7-11.js
+++ coures2./7-11.js
@@ -7,3 +7,17 @@ moveForward(100);
 turnLeft(90);
 moveForward(100);
 ReplayContinue
+
+
+penColour('#000000');
+moveForward(200);
+turnLeft(90);
+moveForward(100);
+turnLeft(90);
+moveForward(200);
+turnLeft(90);
+moveForward(100);
+turnLeft(90);
+moveForward(100);
+turnLeft(90);
+moveForward(100);
