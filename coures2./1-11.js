9b5a87164e1e528d9d38554a06f715b4658be540
LightningDash6 <daniel@behuman.org>
2017-06-05 11:51:27 +0100

    Update 1-11.js

--- coures2./1-11.js
+++ coures2./1-11.js
@@ -146,3 +146,15 @@ moveForward();
 turnRight();
 for (var count3 = 0; count3 < 2; count3++) {
   moveForward();
+
+
+for (var count = 0; count < 2; count++) {
+  moveForward();
+}
+turnLeft();
+for (var count2 = 0; count2 < 2; count2++) {
+  moveForward();
+}
+
+
+
